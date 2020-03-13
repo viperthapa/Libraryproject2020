87e8b62835230c8d8e33d0e87faf803027e477db
ramthapa <thaparam78666@gmail.com>
2020-03-13 11:38:45 +0545

    settings added

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h%ns)wxdigph3a$u_uq0b40oqo-&y^!0^b2hnj=3fas1i^0f5o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
